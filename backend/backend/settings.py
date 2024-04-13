bf47a3ada52fc5e94ba725d209991f2768a481ae
Максим Давыдов <m.davidov92@gmail.com>
2024-04-13 23:46:20 +0300

    Rebuild image

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,16 +106,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
